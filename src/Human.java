4d148350a1bc555c70d3de5e650b7ba757bb9bc7
Kimrayt <kimrayt61@gmail.com>
2023-12-02 13:40:02 +0600

    set Home

--- src/Human.java
+++ src/Human.java
@@ -1,21 +1,82 @@
 public class Human {
-    String name;
-    int age;
-    int height ;
-    int weight;
+    private String heard;
+    private String name;
+    private int age;
+    private int height;
+    private int weight;
 
-    public void setParameters(String name, int age, int height, int weight){
-        this.name = name;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
+//    public Human() {
+//    }
+
+//    public Human(String name, int age, int height, int weight, String heard){
+//        this.name = name;
+//        this.age = age;
+//        this.height = height;
+//        this.weight = weight;
+//        this.heard = heard;
+//    }
+//public Human( String heard){
+//    this.heard = heard;
+//    this.height = 25;
+//    this.weight = 3;
+//}
+
+//    public void setParameters(String name, int age, int height, int weight) {
+//        this.name = name;
+//        this.age = age;
+//        this.height = height;
+//        this.weight = weight;
+//
+//
+//    }
+//
+//    public void tellAboutYourself() {
+//        System.out.println("Я человек ");
+//        System.out.println("И меня зовут: " + this.name);
+//        System.out.println("Мой возраст: " + this.age);
+//        System.out.println("Мой рост: " + this.height);
+//        System.out.println("Мой вес: " + this.weight);
+//        System.out.println("Мои органы : " + this.heard);
+//    }
+
+
+    public String getHeard() {
+        return heard;
     }
 
-    public void tellAboutYourself(){
-        System.out.println("Я человек ");
-        System.out.println("И меня завут: " + this.name);
-        System.out.println("Мой возраст: " + this.age);
-        System.out.println("Мой рост: " + this.height);
-        System.out.println("Мой вес: " + this.weight);
+    public void setHeard(String heard) {
+        this.heard = heard;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,40 @@
 
 public class Main {
     public static void main(String[] args) {
-        Human[] humans = new Human[5];
-        humans[0] = new Human();
-        humans[0].setParameters("Абдиль", 18, 170, 65);
-        humans[1] = new Human();
-        humans[1].setParameters("Мама", 53, 170, 45);
-        humans[2] = new Human();
-        humans[2].setParameters("Папа", 60, 175, 90);
-        humans[3] = new Human();
-        humans[3].setParameters("Захадин", 27, 170, 80);
-        humans[4] = new Human();
-        humans[4].setParameters("Саид", 25, 165, 60);
+//        Human[] humans = new Human[5];
+////        humans[0] = new Human();
+////        humans[0].setParameters("Абдиль", 18, 170, 65);
+////        humans[1] = new Human();
+////        humans[1].setParameters("Мама", 53, 170, 45);
+////        humans[2] = new Human();
+////        humans[2].setParameters("Папа", 60, 175, 90);
+////        humans[3] = new Human();
+////        humans[3].setParameters("Захадин", 27, 170, 80);
+////        humans[4] = new Human();
+////        humans[4].setParameters("Саид", 25, 165, 60);
+//
+//        humans[0] = new Human("Абдиль", 18, 170, 65);
+//        humans[1] = new Human("Мама", 53, 170, 45);
+//        humans[2] = new Human("Папа", 60, 175, 90);
+//        humans[3] = new Human("Захадин", 27, 170, 80);
+//        humans[4] = new Human("Саид", 25, 165, 60);
+//
+//
+//        humans[0].tellAboutYourself();
+//
+//        Home home = new Home();
+//        home.humans = humans;
+//        home.address = "Город бишкек улица 7-й Микрорайон до 23/1";
+//        home.rooms = 5;
+//
+//        home.tellWhoLivesInTheHouse();
 
-        Home home = new Home();
-        home.humans = humans;
-        home.address = "Город бишкек улица 7-й Микрорайон до 23/1";
-        home.rooms = 5;
+        // ================================================
 
-        home.tellWhoLivesInTheHouse();
+        Human man = new Human( );
+        man.setName("Alex");
+        System.out.println(man.getName());
+
 
 
     }
